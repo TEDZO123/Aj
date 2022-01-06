c013de9e48ae6ec04a2903a1898c6b3f0a9b9ebb
TEDZO123 <94947774+TEDZO123@users.noreply.github.com>
2022-01-06 18:31:59 +0530

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -10,24 +10,24 @@ Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message
     var r_text = new Array ();
     
     
-    r_text[0] = "https://i.ibb.co/Y2mXdR5/ajuser.jpg";
+    r_text[0] = "https://i.imgur.com/9nXALw8.jpeg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `ＡＪＵＳＥＲ ＢＯＴ
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `T E D Z O  ＢＯＴ
 
-*bot making video* : _https://youtu.be/MPoRRTkjuW0_
+*BGM BOT GROUP* : https://chat.whatsapp.com/KzPohCK51Tt0Ok4Gt0N12T
 
-*owner id instagram* :https://instagram.com/__ajuz___001?utm_medium=copy_link.
+*owner id instagram* :https://instagram.com/a_s_l_a_m_._x?utm_medium=copy_link.
 
-*github* : _https://github.com/aju001/Ajuser_.
+*OWNER* : https://wa.me/917736835721
 
 ■□■□■□■□■□■□■□■□■□■□
-       _𝘼𝙟𝙪𝙨𝙚𝙧 𝘽𝙤𝙩_
-  ▣▣ created by Aju ▣▣
+       _𝑻𝑬𝑫𝒁𝑶 𝘽𝙤𝙩_
+  ▣▣ created by ASLU ▣▣
 
 `}) 
 
